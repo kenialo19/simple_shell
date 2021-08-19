67abb1322e8f7372f79b4a57df1c2d0ee48b5b4f
Shirley <shircar16@gmail.com>
2021-08-18 23:06:16 -0500

    handle command lines

---
+++ token.c
@@ -0,0 +1,36 @@
+#include "shell.h"
+
+/**
+ * token_command - get token of the string
+ * @line: string
+ * 
+ * 
+ * Return: integer
+ */
+ 
+char **token_command(char *line)
+{
+	char **token = NULL;
+	char *space = " \n";
+	int size;
+	int i = 0;
+
+	if (line == NULL)
+	{
+		return (NULL);
+	}	
+	size = count_word(line);
+	
+	token = malloc(sizeof(char *) * size);
+	if (!token)
+	{
+		return (NULL);
+	}
+	token[0] = strtok(line, space);
+
+	for (i = 1; i < size; i++)
+	{
+		token[i] = strtok(NULL, space);
+	}
+	return(token);
+}
