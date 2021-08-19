1bd7483b8441a0816bb908520b3c984f3620eb72
Shirley <shircar16@gmail.com>
2021-08-18 23:10:30 -0500

    infinite cicle and one write eliminated

--- prompt.c
+++ prompt.c
@@ -13,17 +13,13 @@ char *read_line()
 	size_t linez = 0;
 	int c = 0;
 
-	while (1)
-	{
-		write(STDOUT_FILENO, prompt, _strlen(prompt));
-		c = getline(&line, &linez, stdin);
+	write(STDOUT_FILENO, prompt, _strlen(prompt));
+	c = getline(&line, &linez, stdin);
 
-		if (getline(&line, &linez, stdin) == -1)
-		{
-			free(line);
-			return (NULL);
-		}
-		write(STDOUT_FILENO, line, c);
+	if (c == -1)
+	{
+		free(line);
+		return (NULL);
 	}
 	return (line);
 }
