6b58d07e58d4706c2c9f1a5dd76768e1ebf74e1a
Shirley <shircar16@gmail.com>
2021-08-19 21:56:00 -0500

    get environ variable

---
+++ _getenv.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+
+/**
+ * _getenv - get a environ variable.
+ * @name: environ variable
+ *
+ * Return: Always int.
+ */
+
+char *_getenv(const char *name)
+{
+    int i = 0;
+    char *vble, *var;
+    int a = 0;
+    
+    a = _strlen((char*)name);
+
+    for (; environ[i] != NULL; i++)
+    {
+        vble = environ[i];
+		var = _strchr(vble, '=');
+
+        if (var && (((var - vble) == a) && !_strncmp(vble, (char*)name, a)))
+        {
+            return (var + 1);
+        }
+    }
+    return (NULL);
+}
