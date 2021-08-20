274167f489f34123ff7bbaeb72dd64303ae610b3
Shirley <shircar16@gmail.com>
2021-08-19 21:57:47 -0500

    more str functions

---
+++ str2.c
@@ -0,0 +1,46 @@
+#include "shell.h"
+
+/**
+ * _strchr - localate a character in a string.
+ * @s: string
+ * @c: character
+ *
+ * Return: Always 0.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int j;
+
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		if (s[j] == c)
+			return (&s[j]);
+	}
+	return (0);
+}
+
+/**
+ * _strncpy - copies a string in long array.
+ * @str1: string 1
+ * @str2: string 2
+ * @n: The maximum number of characters to be compared.
+ *
+ * Return: Always 0.
+ */
+
+int _strncmp(const char *str1, const char *str2, size_t n)
+{
+	size_t m;
+	size_t p;
+
+	for (m = 0; str1[m] != '\0' && m < n; m++)
+	{
+		p = str1[m] - str2[m];
+		if (p != 0)
+		{
+			return (p);
+		}
+	}
+	return (0);
+}
