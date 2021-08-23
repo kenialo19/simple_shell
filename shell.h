f5d7a4ec8ae12712695dc422780fe08506631bbe
kenialo19 <3396@holbertonschool.com>
2021-08-23 18:23:20 -0500

    add prototypes

--- shell.h
+++ shell.h
@@ -9,11 +9,14 @@
 #include <sys/stat.h>
 #include <stddef.h>
 #include <string.h>
-
+#include <errno.h>
 
 extern char **environ;
+char *msg;
+int count;
 
 /* PROTYPES FUNCTION_STRC */
+
 int _strlen(char *s);
 int _strcmp(char *s1, char *s2);
 char *_strcpy(char *dest, char *src);
@@ -21,14 +24,19 @@ char *_strchr(char *s, char c);
 int _strncmp(const char *str1, const char *str2, size_t n);
 char *_strdup(const char *s);
 int count_word(char *str);
-char *read_line(void);
+
+/* PROTYPES SHELL */
+
+char *_getenv(const char *name);
+char *read_line();
 char **token_command(char *line);
 int child_exec(char **arg);
 char *builtin(char *cmd, char *dir);
 char *_path_dir(char *cmd);
 
+/* PROTOTYPES BUILTIN */
 
-/* PROTYPES ENVIRON */
-char *_getenv(const char *name);
+/* PROTYPES ERROR */
+void _error(int no_error);
 
 #endif /* _SHELL_H_*/
