0cdafd824903eab6e684bf209133096cea5442c2
kenialo19 <3396@holbertonschool.com>
2021-08-23 18:28:02 -0500

    improving code

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# simple_shell
+# 0x16. C - Simple Shell 
+
+## Description  📑
+
+shell is a command line interpreter, it is the computer program that provides a user interface to access the operating system services. Depending on the type of interface they use, shells can be of various types, in this case, a shell program of type sh (Bourne Shell) will be developed. Users usually interact with a shell through a terminal emulator that is used to enter data into a computer or computer system and display or print it.
+
+
--- prompt.c
+++ prompt.c
@@ -12,22 +12,27 @@ char *read_line()
 	char *prompt = "chispun$ ";
 	size_t linez = 0;
 	int c = 0;
-	
+
 	/*printf("isaty:[%d]\n", isatty(STDIN_FILENO));*/
 	if (isatty(STDIN_FILENO) != 0)
 		write(STDOUT_FILENO, prompt, _strlen(prompt));
 	c = getline(&line, &linez, stdin);
 
+	/*printf("read_line-line=[%s]-linez=[%ld]-c=[%d]\n", line, linez, c);*/
+
 	if (feof(stdin))
 	{
+		/*printf("read_line-Entro feof\n");*/
 		exit(EXIT_SUCCESS);
 	}
 
 	if (c == -1)
 	{
+		printf("read_line-c == -1\n");
 		free(line);
 		return (NULL);
 	}
+
+    /*printf("read_line-SALIDA(line)\n");*/
 	return (line);
 }
-
