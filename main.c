483daa42b953ea8dbfee5ebefbe9e547c388e6c7
kenialo19 <3396@holbertonschool.com>
2021-08-23 18:26:39 -0500

    modified file

--- main.c
+++ main.c
@@ -1,29 +1,41 @@
 #include "shell.h"
-char **token_command(char *line);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
 /**
  * main - execute functions.
  *
  * Return: Void.
  */
+
 int main(void)
 {
 	char *line;
 	char **arg;
-	char *path_value;
-	int j;
 
-
+	count = 0;
 	while (1)
 	{
 		line = read_line();
+		msg = malloc(sizeof(char) * _strlen(line) + 1);
+		_strcpy(msg, line);
+		count++;
+		/*printf("line- main->[%s]", line);
+		printf("msg- main->[%s]", msg);*/
+		if (line == NULL)
+		{
+			printf("line- main->[%s]", line);
+			return (-1);
+		}
+
 		arg = token_command(line);
-		for (j = 0; arg[j]; j++)
-			//printf("%s\n", arg[j]); /* print arguments in separate lines*/
+		/* for (j = 0; arg[j]; j++) printf("%s\n", arg[j]); print arguments in separate lines*/
 		child_exec(arg);
+		free(msg);
 	}
 	free(line);
 	free(arg);
 	return (0);
 }
-
