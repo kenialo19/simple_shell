b3544efc571d94c9ded1f36858faf21c0fae3362
Shirley <shircar16@gmail.com>
2021-08-25 01:07:51 -0500

    fix chispun$ with enter

--- main.c
+++ main.c
@@ -15,34 +15,35 @@ int main(void)
 	char *line;
 	char **arg;
 	int (*b)();
+	char *msg;
+	int count;
 
 	count = 0;
 	while (1)
 	{
-		line = read_line();
-		msg = malloc(sizeof(char) * _strlen(line) + 1);
-		_strcpy(msg, line);
+			line = read_line();
 		count++;
-
-		if (line == NULL)
+		if (line != NULL && line[0] != '\n')
 		{
-			return (-1);
-		}
-		arg = token_command(line);
+			msg = malloc(sizeof(char) * _strlen(line) + 1);
+			_strcpy(msg, line);
 
-		b = _get_built(arg[0]);
-		if (b)
-		{
-			if (b() == 1)
+			arg = token_command(line);
+
+			b = _get_built(arg[0]);
+			if (b)
 			{
-				return (1);
+				if (b() == 1)
+				{
+					return (1);
+				}
+				continue;
 			}
-			continue;
+			child_exec(arg, msg, count);
+			free(msg);
+			free(arg);
+			free(line);
 		}
-		child_exec(arg);
-		free(msg);
-		free(arg);
-		free(line);
 	}
 	return (0);
 }
